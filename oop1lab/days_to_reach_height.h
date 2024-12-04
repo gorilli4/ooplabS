bf7d86e5c8736036867de3bb080be06e005f3af5
gorilli4 <145994444+gorilli4@users.noreply.github.com>
2024-12-04 16:23:52 +0300

    Delete oop1lab directory

--- oop1lab/days_to_reach_height.h
+++
@@ -1,7 +0,0 @@
-#ifndef DAYS_TO_REACH_HEIGHT_H
-#define DAYS_TO_REACH_HEIGHT_H
-
-// Объявление функции
-int daysToReachHeight(int upSpeed, int downSpeed, int needHeight);
-
-#endif // DAYS_TO_REACH_HEIGHT_H
